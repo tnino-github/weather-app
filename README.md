0952fab8d2346f2d5f53dbd2806faf251a0e2acc
tnino <tninomiya.119@outlook.jp>
2020-01-02 22:36:46 +0900

    push

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# weather-app a
+# weather-app
---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Titles extends React.Component {
+  render(){
+    return (
+      <div className="title-container__title">
+        <h1 className="title-container__subtitle">Weather Finder</h1>
+        <h3>Find out temperature, conditions and more...</h3>
+      </div>
+    )
+  }
+};
+
+export default Titles;
